c02626ff8063c7616b9b57cde4da0277c3237368
amyeshreeve <amyeshreeve@gmail.com>
2021-09-30 12:43:05 -0400

    initial HW6 committ

---
+++ Homework6.R
@@ -0,0 +1,30 @@
+## Imports the appropriate data from the shared datasets. 
+
+library(tidyverse)
+library(tidytext)
+library(textstem)
+df = read_csv("datasets/gop_debates.csv")
+
+## Conducts a lemmatized framegram analysis comparing each of the candidates.  
+
+df = df %>%
+  separate(text, c(NA,"text"), sep = ":")
+df$text = lemmatize_strings(df$text)
+
+# Stopwords
+
+stop_words_bounded <- paste0("\\b", stop_words$word, "\\b", collapse = "|")
+
+## Visualizes the top 10 most frames for each of the above named candidates. 
+
+df %>%
+  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
+  count(trigram, sort = TRUE) %>%
+  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
+  mutate(trigram = reorder(trigram, n)) %>%
+  group_by(who) %>% 
+  slice(1:10) %>% # top 10 
+  ggplot(aes(x=trigram, y=n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
