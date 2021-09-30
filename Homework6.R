2c175fe5ed2435f84152279296934c6e00d4c6c0
amyeshreeve <amyeshreeve@gmail.com>
2021-09-30 13:15:38 -0400

    roughly plotted; quite ugly though

--- Homework6.R
+++ Homework6.R
@@ -8,7 +8,9 @@ df = read_csv("datasets/gop_debates.csv")
 ## Conducts a lemmatized framegram analysis comparing each of the candidates.  
 
 df = df %>%
-  separate(text, c(NA,"text"), sep = ":")
+  separate(text, c(NA,"text"), sep = ":") %>%
+  filter(who == "TRUMP" | who == "RUBIO"|who == "CRUZ")
+df$text = stem_strings(df$text)
 df$text = lemmatize_strings(df$text)
 
 # Stopwords
@@ -18,12 +20,13 @@ stop_words_bounded <- paste0("\\b", stop_words$word, "\\b", collapse = "|")
 ## Visualizes the top 10 most frames for each of the above named candidates. 
 
 df %>%
+# filter(who == "TRUMP")
   unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
+  group_by(who) %>%
   count(trigram, sort = TRUE) %>%
   filter(str_count(trigram,stop_words_bounded) < 1) %>% 
   mutate(trigram = reorder(trigram, n)) %>%
-  group_by(who) %>% 
-  slice(1:10) %>% # top 10 
+  slice(1:10) %>%
   ggplot(aes(x=trigram, y=n)) +
   geom_col() +
   xlab(NULL) +
