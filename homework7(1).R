88c2cccab82b4c2809bfd73a6008ef870a1f4a56
amyeshreeve <amyeshreeve@gmail.com>
2021-10-10 18:40:53 -0900

    third HW7 commit

--- homework7(1).R
+++ homework7(1).R
@@ -11,23 +11,25 @@ library(dabestr)
 df = read_csv("gop_debates.csv")
 nrc <- get_sentiments("nrc")
 
-# Removing speaker names, filtering speakers, and unnesting tokens
+# Removing speaker names and filtering speakers
 data = df %>%
   separate(text, c(NA,"text"), sep = ":") %>%
-  filter(who == "BUSH" | who == "CRUZ"|who == "FIORINA"|who == "TRUMP") %>%
+  filter(who == "BUSH" | who == "CRUZ"|who == "FIORINA"|who == "TRUMP")
+
+# Making a column of row names and unnesting tokens.
+data <- cbind(turn = rownames(data), data) %>%
   unnest_tokens(word, text, token = "words")
 
-# Making the index into a column
-data <- cbind(turn = rownames(data), data)
-
-# Lemmatizing
+# Lemmatizing (decided on this method after comparing to stem/regex)
 
 ## df_stemmed <- df %>% mutate(word=stem_words(word))
 ## nrc_stemmed <- nrc %>% mutate(word=stem_words(word))
 ## data1 = df_stemmed %>% left_join(nrc_stemmed)
 
+# Lemmatizes nrc & data, combines them together
 data_lemma <- data %>% mutate(word=lemmatize_words(word))
 nrc_lemma  <- nrc %>% mutate(word=lemmatize_words(word))
+data_lemma  <- data_lemma %>% left_join(nrc_lemma)
 
 # data3 = df %>% regex_left_join(nrc) %>% group_by(word.x)
 
@@ -36,10 +38,13 @@ nrc_lemma  <- nrc %>% mutate(word=lemmatize_words(word))
 data_n <- data_lemma %>%
   count(turn, word, sort = TRUE)
 
-data_n <- data_n %>% left_join(data_lemma)
+data_lemma <- data_lemma %>%
+  filter(!is.na(sentiment))
+
+data_n <- data_n %>% inner_join(data_lemma)
 
 total_words <- data_n %>% 
-  group_by(turn) %>% 
+  group_by(turn) %>%
   summarize(total = sum(n))
 
 data_n <- left_join(data_n, total_words)
@@ -64,16 +69,11 @@ fear_data_n <- data2 %>%
 
 fear_data_n <- inner_join(data2, total_words)
 
-# Average fear frequency grouped by candidate
-
-candidate_n <- data %>%
-  count(who, turn, sort = TRUE) %>%
-  group_by(who) %>%
-  summarise(turns = sum(n))
+# Average fear frequency per turn grouped by candidate
 
 candidate_analysis <- fear_data_n %>%
   group_by(who) %>%
-  summarise(value = sum(tf))
+  summarise(value = mean(tf))
 
 candidate_analysis = candidate_analysis %>% left_join(candidate_n)
 
