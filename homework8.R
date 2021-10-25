6dff6b85425dff487b441b16a89bc42c51dfb511
amyeshreeve <amyeshreeve@gmail.com>
2021-10-25 05:19:59 -0900

    monday commit

--- homework8.R
+++ homework8.R
@@ -1,24 +1,23 @@
 # Write an R script that performs an automated summative content analysis.
 
+# Read in libraries
 library(tidyverse)
 library(tidytext)
 library(stringr)
 library(textstem)
 library(irr)
 
-
 reddit <- read_csv("utreddit.csv")
+reddit <- reddit %>%
+  unite("post_text", title:post_text, remove = TRUE)
 
-sched_words <- ("course|professor|schedule|time|flag|easy|grades|fail|
-                instructor|conflict|tips|core|credit|register")
-major_words <- ("major|accepted|department|program|transfer|internal")
-policy_words <- ("claiming|wifi|syllabus|gpa|waitlist|AP|IB")
-finaid_words <- ("money|pell|federal|bill|fafsa|federal|grant|loan|
-                  scholarship|debt|cost|expensive|tuition")
-housing_words <- ("dorm|roommate|hall|housing|occupancy|room|bedroom| 
-                  apartment|rent|maintenance")
-food_words <- ("eat|food|kins|jester|dining|meal|grub|ate|lunch|breakfast|
-               |dessert|dinner|snack")
+# Terms based on topic
+sched_words <- ("course|professor|schedule|flag|easy|grades|fail|instructor|conflict|tips|core|register|class|how is|teach")
+major_words <- ("accepted|department|program|transfer|internal")
+policy_words <- ("claiming|wifi|syllabus|gpa|waitlist|ap|ib|print|closed|sat")
+finaid_words <- ("money|pell|federal|bill|fafsa|federal|grant|loan|scholarship|debt|cost|expensive|tuition")
+housing_words <- ("dorm|roommate|hall|housing|occupancy|room|bedroom|apartment|rent|maintenance")
+food_words <- ("food|kins|jester|dining|meal|grub|lunch|breakfast|dessert|dinner|snack")
 entertain_words <- ("game|football|gym|play|union|club|party|friend|fun")
 humor_words <- ("domino|lol|lmao|fun|meme")
 other_words <- ("sell|work|job|technology|canvas")
@@ -52,9 +51,23 @@ bot_reddit <- reddit %>%
          bot_housing = ifelse(str_detect(post_text, housing_words_regex),1,0),
          bot_food = ifelse(str_detect(post_text, food_words_regex),1,0),
          bot_entertain = ifelse(str_detect(post_text, entertain_words_regex),1,0),
-         bot_humor = ifelse(str_detect(post_text, humor_words_regex),1,0),
-         bot_other = ifelse(bot_sched + bot_major + bot_policy + bot_finaid +
-                              bot_housing + bot_food + bot_entertain +
-                              bot_humor == 0), 1,0)
-
+         bot_humor = ifelse(str_detect(post_text, humor_words_regex),1,0))
+     
+# 'Other'
 # I understand that the bot_other is making it harder on myself
+bot_reddit$total_cats = rowSums(bot_reddit[,c(12,13,14,15,16,17,18,19)])
+bot_reddit$bot_other = ifelse(bot_reddit$total_cats == 0, 1, 0)
+
+
+## Code to check accuracy of terms (switching out variables to avoid bloat)
+
+# Check agreement 
+agree(data.frame(bot_reddit$ policy, bot_reddit$bot_policy))
+
+# Check reliability 
+kappa2(data.frame(bot_reddit$ policy, bot_reddit$bot_policy))
+
+# Diagnose issues 
+bot_reddit %>% 
+  mutate(agree = policy + bot_policy) %>% 
+  filter(agree == 1)
