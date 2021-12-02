27ec6d8e6022af6ddc98fb3cdf3fb8418e3205ca
amyeshreeve <amyeshreeve@gmail.com>
2021-12-02 12:43:12 -1000

    second metric commit

--- ShreeveMetric.R
+++ ShreeveMetric.R
@@ -35,89 +35,41 @@ write.xlsx(ldscontent, 'LDSData.xlsx')
 
 # Importing datasets
 
-ex = read_excel("ExMormonData.xlsx")
-lds = read_excel("LDSData.xlsx")
+ex = read_excel("ExMormonData.xlsx") %>% na.omit()
+lds = read_excel("LDSData.xlsx")  %>% na.omit()
 
-# Skill 2 -- Framegrams
+# Skill 2 & 3 -- Framegrams & ggplot
+
 # Lemmatizing
 
-ex$comment = lemmatize_words(ex$comment)
+lemmatize_strings("gay lesbian bisexual transgender queer lgbt")
+lgbt <- "gay|lesbian|bisexual|transgender|queer|lgbt"
 
-ex_word_n <- ex %>%
-  unnest_tokens(word, comment) %>%
-  anti_join(stop_words) %>%
-  count(word, sort = TRUE)
+# Bound stopwords
+stop_words_bounded <- paste0("\\b", stop_words$word, "\\b", collapse = "|")
 
-# Get total words for tf/if calc
-
-ex_total_words <- ex_word_n %>% 
-  summarize(total = sum(n))
-
-ex_word_data <- left_join(ex_word_n, ex_total_words)
-
-# Save word data with previous data and tf_idf
-
-ex_word_data <- ex_word_data %>%
-  bind_tf_idf(word, date, n)
-
-# Print word data
-
-ex_word_data %>%
-  select(-total) %>%
-  arrange(desc(tf_idf))
-
-# Skill 3 -- Visualizing with ggplot
-# Visualizes the top 10 most common terms.
-
-ex_word_data %>%
-  arrange(desc(tf_idf)) %>%
-  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
-  slice(1:10) %>% 
-  ggplot(aes(word, tf_idf, fill = date)) +
-  geom_col(show.legend = FALSE) +
-  labs(x = NULL, y = "tf-idf") +
-  facet_wrap(~date, ncol = 2, scales = "free") +
+ex %>%
+  unnest_tokens(trigram, comment, token = "ngrams", n=3) %>% 
+  count(trigram, sort = TRUE) %>%
+  filter(str_detect(trigram,lgbt)) %>% 
+  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
+  mutate(trigram = reorder(trigram, n)) %>%
+  slice(1:15) %>%
+  ggplot(aes(x=trigram, y=n)) +
+  geom_col() +
+  xlab(NULL) +
   coord_flip()
 
-# Now, let's do it all again for the other subreddit
-
-# Lemmatizing
-
-lds$comment = lemmatize_words(lds$comment)
-
-lds_word_n <- lds %>%
-  unnest_tokens(word, comment) %>%
-  anti_join(stop_words) %>%
-  count(word, sort = TRUE)
-
-# Get total words for tf/if calc
-
-lds_total_words <- lds_word_n %>% 
-  summarize(total = sum(n))
-
-lds_word_data <- left_join(lds_word_n, lds_total_words)
-
-# Save word data with previous data and tf_idf
-
-lds_word_data <- lds_word_data %>%
-  bind_tf_idf(word, date, n)
-
-# Print word data
-
-lds_word_data %>%
-  select(-total) %>%
-  arrange(desc(tf_idf))
-
-# Visualizes the top 10 most common terms. 
-
-lds_word_data %>%
-  arrange(desc(tf_idf)) %>%
-  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
-  slice(1:10) %>% 
-  ggplot(aes(word, tf_idf, fill = date)) +
-  geom_col(show.legend = FALSE) +
-  labs(x = NULL, y = "tf-idf") +
-  facet_wrap(~date, ncol = 2, scales = "free") +
+lds %>%
+  unnest_tokens(trigram, comment, token = "ngrams", n=3) %>% 
+  count(trigram, sort = TRUE) %>%
+  filter(str_detect(trigram,lgbt)) %>% 
+  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
+  mutate(trigram = reorder(trigram, n)) %>%
+  slice(1:15) %>%
+  ggplot(aes(x=trigram, y=n)) +
+  geom_col() +
+  xlab(NULL) +
   coord_flip()
 
 # Skill 4 -- ML Classification
@@ -129,14 +81,27 @@ library(tidytext)
 library(caret)
 library(pROC)
 
-df = read_csv("spam_ham.csv") %>% mutate(number=row.names(.))
-df_labels <- df %>% mutate(spam = ifelse(Type=="spam","yes","no")) %>% select(number, spam)
+# Had to simplify the data because the size kept crashing r lol
+
+lds2 <- lds %>% select(c("post_text", "subreddit")) %>% unique()
+lds2 <- lds2[1:50, ]
+ex2 <- ex %>% select(c("post_text", "subreddit")) %>% unique()
+ex2 <- ex2[1:50, ]
+
+df <- rbind(lds2, ex2) %>% 
+  select(c("post_text", "subreddit")) %>% 
+  mutate(number=row.names(.))
+
+# Making labels to save for later
+
+df_labels <- df %>% mutate(type = ifelse(subreddit=="latterdaysaints","yes","no")) %>%
+  select(number, type)
 
 # Feature Engineering
 # TF-IDF is appropriate because we don't already know the content categories.
 
 df_counts <- map_df(1:2, 
-                    ~ unnest_tokens(df, word, Message, 
+                    ~ unnest_tokens(df, word, post_text, 
                                     token = "ngrams", n = .x)) %>%
   anti_join(stop_words, by = "word") %>%
   count(number, word, sort = TRUE)
@@ -163,7 +128,7 @@ df_engineered <- df_dtm %>%
   as.matrix() %>% 
   as.data.frame() %>% 
   mutate(number = dimnames(df_dtm)[[1]]) %>% 
-  right_join(df_labels) %>% 
+  right_join(df_labels, by = "number") %>% 
   filter(complete.cases(.))
 
 # Training and test sets
@@ -178,44 +143,50 @@ training_set_no_id <- df_engineered %>%
 
 # KNN Modelling
 
-knnfit <- train(spam ~ ., 
+knnfit <- train(type ~ ., 
                 data = training_set_no_id,
                 method = "knn",
                 tuneLength = 7)
-knn_pred <- test_set %>% select(-spam) %>% predict(knnfit, newdata = ., type = 'prob')
+knn_pred <- test_set %>% select(-type) %>% predict(knnfit, newdata = ., type = 'prob')
 
 # NB Modelling
 
 fitControl <- trainControl(method = "repeatedcv", 
-                           number = 10, 
-                           repeats = 5, 
+                           number = 5, 
+                           repeats = 3, 
                            classProbs = TRUE, 
                            summaryFunction = twoClassSummary)
-nb_mod = train(spam ~ ., 
+nb_mod = train(type ~ ., 
                data = training_set_no_id, 
                method="naive_bayes", 
                trControl = fitControl, 
                tuneGrid = expand.grid(usekernel=TRUE,laplace=0,adjust=1))
 
-nb_pred <- test_set %>% select(-spam) %>% predict(nb_mod, newdata = ., type = 'prob')
+nb_pred <- test_set %>% select(-type) %>% predict(nb_mod, newdata = ., type = 'prob')
 
 # NN Modelling
 
-nnetFit <- train(spam ~ ., 
+nnetFit <- train(type ~ ., 
                  data = training_set_no_id,
                  method = "nnet",
                  metric = "ROC",
                  trControl = fitControl,
                  tuneLength = 3,
+                 MaxNWts = 9352,
                  verbose = FALSE)
 
-nn_pred <- test_set %>% select(-spam) %>% predict(nnetFit, newdata = ., type = 'prob')
+nn_pred <- test_set %>% select(-type) %>% predict(nnetFit, newdata = ., type = 'prob')
 
 # ROCs
 
-knn_roc <- roc(test_set$spam,knn_pred$yes)
-nb_roc <- roc(test_set$spam,nb_pred$yes)
-nn_roc <- roc(test_set$spam,nn_pred$yes)
+knn_roc <- roc(test_set$type,knn_pred$yes)
+nb_roc <- roc(test_set$type,nb_pred$yes)
+nn_roc <- roc(test_set$type,nn_pred$yes)
+
+# Compare numerically
+knn_roc
+nb_roc
+nn_roc
 
 # Compare visually
 
